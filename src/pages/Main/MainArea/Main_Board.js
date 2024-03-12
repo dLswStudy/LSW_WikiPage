21fc4e7242440fb056e6ecc1e8238dc9716a232a
dLswStudy <devLswStudy@gmail.com>
2024-03-12 12:09:49 +0900

    무한스크롤 적용

--- src/pages/Main/MainArea/Main_Board.js
+++ src/pages/Main/MainArea/Main_Board.js
@@ -1,20 +1,31 @@
 import SpinByW from "src/components/SpinByW/SpinByW";
 import Board from "src/components/Board/Board";
-import React from "react";
+import React, {useCallback, useEffect, useRef} from "react";
 import axios from "axios";
 import appConfig from "src/config/app.config";
-import {useQuery} from "react-query";
+import {useInfiniteQuery, useQuery} from "react-query";
 import {useNavigate} from "react-router-dom";
 
 const api_getPosts = async () => {
     // await new Promise(resolve => setTimeout(resolve, 100000))
     return await axios.get(appConfig.apiPreUrl + '/Post')
 };
-//모바일용
+//모바일용(무한스크롤)
 const api_getPosts_M = async (lastIdx, pageSize) => {
     // await new Promise(resolve => setTimeout(resolve, 100000))
     return await axios.get(appConfig.apiPreUrl + `/Post/infiscroll?lastVisibleIndex=${lastIdx}&pageSize=${pageSize}`)
 };
+const fetchPosts = async ({ pageParam = 0 }) => {
+    const pageSize = 20; // 한 페이지당 항목 수
+    const {data} = await api_getPosts_M(pageParam, pageSize);
+    console.log("fetchPosts posts = ", data);
+
+    return {
+        items: data, // API 응답에서 항목 목록
+        nextCursor: data.length === pageSize ? data[data.length-1].index : undefined // 다음 페이지의 시작 인덱스
+    }
+};
+
 const columns = [
     {title: '번호', dataIndex: 'index', maxWidth: 70},
     {title: '제목', dataIndex: 'title'},
@@ -25,9 +36,31 @@ export function Main_Board(props) {
     console.log('%c Main_Board Rendering', "color:pink");
     const navigate = useNavigate();
 
-    const { data, isLoading, error } = useQuery(['posts'],  api_getPosts_M(null,40));
-    const posts = data?.data
-    const errMsg = error ? '게시물을 가져오는 데 실패했습니다.' : null;
+    const {
+        data,
+        isLoading,
+        isError,
+        error,
+        fetchNextPage,
+        hasNextPage,
+    } = useInfiniteQuery('posts', fetchPosts, {
+        getNextPageParam: (lastPage, allPages) => lastPage.nextCursor,
+    });
+
+    const errMsg = isError ? '게시물을 가져오는 데 실패했습니다.' : null;
+
+    const observer = useRef();
+    const lastPostElementRef = useCallback(node => {
+        if (isLoading) return;
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasNextPage) {
+                fetchNextPage();
+            }
+        });
+        if (node) observer.current.observe(node);
+    }, [isLoading, hasNextPage, fetchNextPage]);
+    const posts = data?.pages.flatMap(page => page.items) ?? [];
 
     const toDetail = (rowData) => {
         navigate('/wiki', {state: {index: rowData.index}})
@@ -36,6 +69,7 @@ export function Main_Board(props) {
     return (
         <SpinByW loading={isLoading} top_px={100}>
             <Board columns={columns} rowDatas={posts} errMsg={errMsg} toDetailFunc={toDetail}/>
+            <div ref={lastPostElementRef} />
         </SpinByW>
     );
 };
